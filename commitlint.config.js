0e9cd789c604fabffe013f24f7253518255d8632
Rouan Wilsenach <rouanw@users.noreply.github.com>
2025-08-05 14:54:07 +0100

    chore: remove git semantic release

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "body-max-line-length": [1, "always", 500],
-    "footer-max-line-length": [1, "always", 500],
-  },
-};
+module.exports = { extends: ["@commitlint/config-conventional"] };
